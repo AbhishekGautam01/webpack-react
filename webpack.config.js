1b31053af5c337434fb868ec931d24dc34970a93
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-02-08 16:57:38 +0530

    Configuring Output Webpack property

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   entry: __dirname + 'app/index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  output: {
+    filename: 'transformed.js',
+    path: __dirname + '/dist',
+  },
 };
