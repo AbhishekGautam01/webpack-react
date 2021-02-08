b096bdc7dc161e47828cf7c8f0f4a852fe63b112
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-02-08 19:06:54 +0530

    Adding HTML WEBPACK CONFIG

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackConfigPlugin = new HTMLWebpackPlugin({
+  template: __dirname + 'app/index.html',
+  filename: 'index.html',
+  inject: 'head',
+});
 module.exports = {
   entry: __dirname + 'app/index.js',
   module: {
@@ -13,4 +19,5 @@ module.exports = {
     filename: 'transformed.js',
     path: __dirname + '/dist',
   },
+  plugins: [HTMLWebpackConfigPlugin],
 };
